44d0a7047cf0b3591f6a59435f71aac0220a5056
Nils Wandel <wandeln@cs.uni-bonn.de>
2026-02-26 14:37:35 +0100

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="AdamWClip",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["torch"],
     author="Nils Wandel",
